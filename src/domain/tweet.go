55bc7ae5ef0aeab89a76eea31f09abf8c71f5983
Darío Reyes <eran6reyes@gmail.com>
2017-11-29 14:38:57 -0300

    Estructura del usuario de tweet.go a user.go

--- src/domain/tweet.go
+++ src/domain/tweet.go
@@ -12,41 +12,7 @@ type Tweet struct {
 	Date *time.Time
 }
 
-// User Type User with its name
-type User struct {
-	Name      string
-	Nick      string
-	Email     string
-	Password  string
-	Following []*User
-}
-
 // NewTweet creates and returns a tweet
 func NewTweet(user *User, text string) *Tweet {
 	return &Tweet{0, user, text, nil}
-}
-
-// NewUser creates and returns a user
-func NewUser(username string, nick string, email string, password string) *User {
-	return &User{username, nick, email, password, make([]*User, 0)}
-}
-
-// Follow Follow another user from service
-func (u *User) Follow(user *User) {
-	u.Following = append(u.Following, user)
-}
-
-// Unfollow Unfollow one user from service
-func (u *User) Unfollow(user *User) {
-	u.Following = deleteFromUserList(u.Following, user)
-}
-
-func deleteFromUserList(userList []*User, user *User) []*User {
-	newList := make([]*User, 0)
-	for _, u := range userList {
-		if user != u {
-			newList = append(newList, u)
-		}
-	}
-	return newList
 }
