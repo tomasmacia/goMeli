4bac9cda84c6fe2b78f7fa2fece0ecf8dabc63d4
Darío Reyes <eran6reyes@gmail.com>
2017-12-01 09:54:00 -0300

    Agrega tests de interfaz

--- src/service/tweet_manager_test.go
+++ src/service/tweet_manager_test.go
@@ -441,3 +441,68 @@ func TestReferences(t *testing.T) {
 		t.Errorf("Original tweet from tweet_manager_test changed when it shouldn't have")
 	}
 }
+
+func TestTextTweetPrintsUserAndText(t *testing.T) {
+
+	// Initialization
+	tweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
+
+	// Operation
+	text := tweet.PrintableTweet()
+
+	// Validation
+	expectedText := "@grupoesfera: This is my tweet"
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
+
+}
+
+func TestImageTweetPrintsUserTextAndImageURL(t *testing.T) {
+
+	// Initialization
+	tweet := domain.NewImageTweet("grupoesfera", "This is my image", "http://www.grupoesfera.com.ar/common/img/grupoesfera.png")
+
+	// Operation
+	text := tweet.PrintableTweet()
+
+	// Validation
+	expectedText := "@grupoesfera: This is my image http://www.grupoesfera.com.ar/common/img/grupoesfera.png"
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
+
+}
+
+func TestQuoteTweetPrintsUserTextAndQuotedTweet(t *testing.T) {
+
+	// Initialization
+	quotedTweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
+	tweet := domain.NewQuoteTweet("nick", "Awesome", quotedTweet)
+
+	// Operation
+	text := tweet.PrintableTweet()
+
+	// Validation
+	expectedText := `@nick: Awesome "@grupoesfera: This is my tweet"`
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
+
+}
+
+func TestCanGetAStringFromATweet(t *testing.T) {
+
+	// Initialization
+	tweet := domain.NewTextTweet("grupoesfera", "This is my tweet")
+
+	// Operation
+	text := tweet.String()
+
+	// Validation
+	expectedText := "@grupoesfera: This is my tweet"
+	if text != expectedText {
+		t.Errorf("The expected text is %s but was %s", expectedText, text)
+	}
+
+}
